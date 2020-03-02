b55e7dad5b69d3b90e818328558d83ec04ea053e
luissj <luislipeaz@hotmail.com>
2020-03-02 09:37:22 -0300

    primeira

---
+++ aula03.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>TO COM DEPRESSÃO</title>
+  </head>
+
+  <style>
+body {
+  background-image: url('nfll.jpg');
+}
+</style>
+
+<body>
+  <style media="screen">
+  .noticias h2{
+    color:#f2f2f2;
+    background-img: #0d0d0d;
+  }
+  </style>
+
+  <style media="screen">
+  .noticias h3{
+    color:#4dd2ff;
+    background-img: #0d0d0d;
+  }
+  </style>
+
+
+<div class="noticias">
+<h2>ESPN - A Melhor Fonte de Notícias Esportivas</h2>
+  <h3>.Tom Brady e Seu Futuro.</h3>
+  <img src="i.jpg" alt="Tom Brady comemora o título do New England Patriots no Super Bowl XXXVI">
+ <p>Segundo Ian Rapoport, da NFL Media, os representantes do camisa 12 se reuniram com
+  Los Angeles Chargers, Las Vegas Raiders e Indianapolis Colts nesta semana em Indianapolis.</p>
+ <p>Tom Brady teve seu contrato encerrado ao final da última temporada e será um agente livre
+    nesta intertemporada, talvez o mais cobiçado da história da NFL.</p>
+  <p>As conversas irão dar uma boa ideia de mercado para os Patriots antes da reunião com sua estrela. Existe, porém,
+     um sentimento cada vez maior dentro da liga de que Brady seguirá seu futuro em outra cidade.</p>
+</div>
+
+<div class="noticias">
+  <h3>.NFL irá aumentar número de times nos Playoffs.</h3>
+  <style>
+table, th, td {
+  border: 1px solid black}
+</style>
+<table style="width:40%">
+  <tr>
+    <th>Conferência Americana </th>
+    <th>Conferência Nacional</th>
+  </tr>
+  <tr>
+    <td>KCC-Chiefs</td>
+    <td>NYG-Giants</td>
+  </tr>
+  <tr>
+    <td>LVR-Raiders</td>
+    <td>NEP-Patriots</td>
+  </tr>
+  <tr>
+    <td>DB-Broncos</td>
+    <td>SF49-49ers</td>
+  </tr>
+</table>
+<p>Na proposta que é esperada para ser aprovada, sete times de cada conferência irão se classificar para os playoffs,
+   aumentando em uma equipe o número de participantes atuais, que são seis de cada conferência.</p>
+<p>Na tabela acima os times que também estariam nos playoffs, Isso resultaria em uma nova pós-temporada,
+  com 14 times ao invés de 12, e seis jogos na semana do wildcard, três no sábado e três no domingo.</p>
+</div>
+
+
+<div class="noticias">
+  Notícia 3: uma imageme 2 paragrafos
+</div>
+
+</div>
+  </body>
+</html>
