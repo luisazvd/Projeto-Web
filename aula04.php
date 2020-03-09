eeae0e40f51d7f6d2c3598e882d3e920578f330c
Aluno <luislipeaz@hotmail.com>
2020-03-09 09:34:03 -0300

    segunda

---
+++ aula04.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Aula 04 - CSS e HTML</title>
+    <link rel="stylesheet" href="aula04.css">
+    <link href="https://fonts.googleapis.com/css?family=Monoton&display=swap" rel="stylesheet">
+  </head>
+  <body>
+<div class="site">
+
+  <div class="topo">
+    Topo do Site
+  </div>
+<div class="conteudo">
+  Corpo do site
+</div>
+
+<div class="rodape">
+  Desenvolvido by Guideiros Harden
+</div>
+
+</div>
+  </body>
+</html>
