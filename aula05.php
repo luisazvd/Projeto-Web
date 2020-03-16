1ec7151c0dfa1f0ba975a5189eb46a89d2eab609
luissj <luislipeaz@hotmail.com>
2020-03-16 09:42:37 -0300

    terceira

---
+++ aula05.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Aula 05 - Menu e Botão</title>
+    <link rel="stylesheet" href="aula05.css">
+    <link href="https://fonts.googleapis.com/css?family=Monoton&display=swap" rel="stylesheet">
+  </head>
+
+
+  <body>
+<div class="site">
+  <div class="topo">
+  <h3>  ESPN - A Melhor Fonte de Notícias Esportivas </h3>
+<div class="menu">
+    <ul>
+      <li><a href="#">Pagina Inicial</a></li>
+      <li><a href="#">Cadastro</a></li>
+      <li><a href="#">Contato</a></li>
+      <li><a href="#">Ajuda</a></li>
+      <li><a href="#">Login</a></li>
+    </ul>
+</div>
+  </div>
+
+<div class="conteudo">
+
+  <div class="noticias">
+    <h4>.Tom Brady e Seu Futuro.</h4>
+    <img src="i.jpg" alt="Tom Brady comemora o título do New England Patriots no Super Bowl XXXVI">
+   <p>Segundo Ian Rapoport, da NFL Media, os representantes do camisa 12 se reuniram com
+    Los Angeles Chargers, Las Vegas Raiders e Indianapolis Colts nesta semana em Indianapolis.</p>
+   <p>Tom Brady teve seu contrato encerrado ao final da última temporada e será um agente livre
+      nesta intertemporada, talvez o mais cobiçado da história da NFL.</p>
+    <p>As conversas irão dar uma boa ideia de mercado para os Patriots antes da reunião com sua estrela. Existe, porém,
+       um sentimento cada vez maior dentro da liga de que Brady seguirá seu futuro em outra cidade.</p>
+  </div>
+<br><br><br>
+
+  <div class="noticias">
+    <h3>.NFL irá aumentar número de times nos Playoffs.</h3>
+    <style>
+  table, th, td {
+    border: 1px solid black}
+  </style>
+  <table style="width:40%">
+    <tr>
+      <th>Conferência Americana </th>
+      <th>Conferência Nacional</th>
+    </tr>
+    <tr>
+      <td>KCC-Chiefs</td>
+      <td>NYG-Giants</td>
+    </tr>
+    <tr>
+      <td>LVR-Raiders</td>
+      <td>NEP-Patriots</td>
+    </tr>
+    <tr>
+      <td>DB-Broncos</td>
+      <td>SF49-49ers</td>
+    </tr>
+  </table>
+  <p>Na proposta que é esperada para ser aprovada, sete times de cada conferência irão se classificar para os playoffs,
+     aumentando em uma equipe o número de participantes atuais, que são seis de cada conferência.</p>
+  <p>Na tabela acima os times que também estariam nos playoffs, Isso resultaria em uma nova pós-temporada,
+    com 14 times ao invés de 12, e seis jogos na semana do wildcard, três no sábado e três no domingo.</p>
+  </div>
+  <h4>Criação de Botões</h4>
+
+  <a href="#" class="botao botao-azul">Butão Azul</a>
+  <a href="#" class="botao botao-verde">Butão Verde</a>
+  <a href="#" class="botao botao-vermelho">Butão Vermelho</a>
+</div>
+
+<div class="rodape">
+  Desenvolvido by Dosto
+</div>
+
+</div>
+  </body>
+</html>
